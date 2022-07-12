86833da5151268f3a87e0c1fabfb63b699a025f0
chamchiking <chamj61047@gmail.com>
2022-07-12 14:55:45 +0900

    민철 leetcode 업로드

--- week2/leetcode/91/mincheol.py
+++ week2/leetcode/91/mincheol.py
@@ -1,1 +1,13 @@
-..
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        N = len(s)
+        answer = [0] * N
+        answer[0] = 1 if 0 < int(s[0]) <= 26 else 0
+
+        for i in range(1, N):
+            print(s[i], s[i - 1 : i + 1])
+            if 0 < int(s[i]) <= 26:
+                answer[i] += answer[i - 1]
+            if 0 < int(s[i - 1 : i + 1]) <= 26 and s[i - 1] != "0":
+                answer[i] += answer[i - 2] if i - 2 >= 0 else 1
+        return answer[-1]
